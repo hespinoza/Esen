a364a07ee07c479ec9fc02ae7087cb0f91438f97
Hector Espinoza <hectoraee@gmail.com>
2014-04-07 00:33:53 -0400

    Inicio
    sync del master

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Esen::Application.config.secret_key_base = 'f882ac7244b346e7d461e4d01beb5c2333cceea02d5ca989b40f355b1f5d757b7997776d5fb55dd29970c0b706ff25656a7fa47a2b3cdf1d5842da64f0c626ba'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Esen::Application.config.session_store :cookie_store, key: '_Esen_session'
